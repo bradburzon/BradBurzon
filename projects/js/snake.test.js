0e87f55f33e6ba974c5e2c8e1a4315f08713e30b
Brad Burzon <bradburzon@ymail.com>
2020-03-11 20:15:31 -0400

    complete set up snake game

--- projects/js/snake.test.js
+++ projects/js/snake.test.js
@@ -1,4 +1,5 @@
-const {setup,resizeCanvas} = require('./snake');
+
+const {setup,getXandY,randomXandY} = require('./snake');
 
 test('setup creates a new canvas', () => {
   document.body.innerHTML = `
@@ -6,12 +7,10 @@ test('setup creates a new canvas', () => {
   <div  id="game">
   </div>
   </div>`;
-
   var game = document.getElementById("game");
   expect(game).toBeDefined();
   game.width = 400;
   game.height = 400;
-
   setup(game.width, game.height);
   var canvas = document.querySelector('.myCanvas');
   //test if canvas exists
@@ -22,14 +21,13 @@ test('setup sets up right dimension', () => {
   document.body.innerHTML = `
   <div class="col" style="padding:0px;">
   <div  id="game">
+  <canvas class="myCanvas" width="400" height="400"></canvas></div>
   </div>
   </div>`;
-
   var game = document.getElementById("game");
   expect(game).toBeDefined();
   game.width = 400;
   game.height = 400;
-
   setup(game.width, game.height);
   var canvas = document.querySelector('.myCanvas');
   //test if canvas has right width and height
@@ -39,64 +37,94 @@ test('setup sets up right dimension', () => {
 
 test('setup doesnt create new canvas if theres existing', () => {
   document.body.innerHTML = `<div class="col" style="padding:0px;"><div id="game"></div></div>`;
-
   var game = document.getElementById("game");
   expect(game).toBeDefined();
   game.width = 400;
   game.height = 400;
-
   setup(game.width, game.height);
   var canvas = document.querySelector('.myCanvas');
   //test if canvas exists
   expect(canvas).toBeDefined();
   expect(document.body.innerHTML).toBe(`<div class="col" style="padding:0px;"><div id="game"><canvas class="myCanvas" width="400" height="400"></canvas></div></div>`);
-
-  //new setup
-  setup(300, 400);
-  expect(document.body.innerHTML).toBe(`<div class="col" style="padding:0px;"><div id="game"><canvas class="myCanvas" width="300" height="400"></canvas></div></div>`);
+  setup(300, 600);
+  expect(document.body.innerHTML).toBe(`<div class="col" style="padding:0px;"><div id="game"><canvas class="myCanvas" width="300" height="600"></canvas></div></div>`);
   expect(canvas.width).toBe(300);
-  expect(canvas.height).toBe(400);
+  expect(canvas.height).toBe(600);
 });
 
+//TESTING | UNCOMMENT to test
+// module.exports = {setup, getXandY, randomXandY};
+// var $ = require('jquery');
 
-test('resizeCanvas change canvas size on resize', () => {
+// test('resizeCanvas changes size', () => {
+//   document.body.innerHTML = `
+//   <div class="col" style="padding:0px;">
+//   <div  id="game">
+//   </div>
+//   </div>`;
+//   var game = document.getElementById("game");
+//   expect(game).toBeDefined();
+//   game.width = 400;
+//   game.height = 400;
+//   setup(game.width, game.height);
+//   var canvas = document.querySelector('.myCanvas');
+//   expect(canvas.width).toBe(400);
+//   expect(canvas.height).toBe(400);
+//   console.log(document.body.innerHTML);
+//
+//   global.dispatchEvent(new Event('resize'));
+//
+//   expect(canvas.width == 400).toBeFalsy();
+//   expect(canvas.height == 400).toBeFalsy();
+//   //use toHaveBeenCalled(1);
+// });
+//
+// // test('resizeCanvas change size correctly', () => {
+// //   document.body.innerHTML = `
+// //   <div class="col" style="padding:0px;">
+// //   <div  id="game">
+// //   </div>
+// //   </div>`;
+// //   var game = document.getElementById("game");
+// //   game.width = 400;
+// //   game.height = 400;
+// //   setup(game.width, game.height);
+// //   var canvas = document.querySelector('.myCanvas');
+// //   var column = document.querySelector('.col');
+// //   column.width = 600;
+// //   column.height = 600;
+// //   global.dispatchEvent(new Event('resize'));
+// //   expect(canvas.width).toBe(600);
+// //   expect(canvas.height).toBe(600);
+// // });
+
+test('getXandY returns coord', () => {
   document.body.innerHTML = `
   <div class="col" style="padding:0px;">
   <div  id="game">
+  <canvas class="myCanvas" width="400" height="400"></canvas>
   </div>
   </div>`;
-  var game = document.getElementById("game");
-  expect(game).toBeDefined();
-  game.width = 400;
-  game.height = 400;
-  setup(game.width, game.height);
-  var canvas = document.querySelector('.myCanvas');
-  expect(canvas.width).toBe(400);
-  expect(canvas.height).toBe(400);
-  global.innerWidth = 600;
-  global.dispatchEvent(new Event('resize'));
-  expect(canvas.width == 400).toBeFalsy();
-  expect(canvas.height == 400).toBeFalsy();
+
+  var click = new Event('click');
+  click.clientX = 100;
+  click.clientY = 200;
+  expect(getXandY(click)).toStrictEqual({x:100,y:200});
 });
 
-test('resizeCanvas change size responsively', () => {
+test('randomXandY gives coord', () => {
   document.body.innerHTML = `
   <div class="col" style="padding:0px;">
   <div  id="game">
+  <canvas class="myCanvas" width="400" height="400"></canvas>
   </div>
   </div>`;
-  var game = document.getElementById("game");
-  expect(game).toBeDefined();
-  game.width = 400;
-  game.height = 400;
-  setup(game.width, game.height);
-  var canvas = document.querySelector('.myCanvas');
-  expect(canvas.width).toBe(400);
-  expect(canvas.height).toBe(400);
-  global.innerWidth = 600;
-  global.dispatchEvent(new Event('resize'));
-  expect(canvas.width == 400).toBeFalsy();
-  expect(canvas.height == 400).toBeFalsy();
+
+  var ran = randomXandY();
+  expect(ran).toBeDefined();
+  expect(ran.x).toBeDefined();
+  expect(ran.y).toBeDefined();
+  // expect(ran).toContain('x');
 });
 
 // test('call resizeCanvas on resize', () => {
