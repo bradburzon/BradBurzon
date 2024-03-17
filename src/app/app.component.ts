cf2e3e952c8e194c634b804ebac68f80223f4ac6
Brad Burzon <bradburzon@ymail.com>
2024-03-16 18:31:54 -0700

    fix routerLinkActive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import {
+  RouterLink,
+  RouterLinkActive,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SkillComponent } from './skill/skill.component';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-import { FooterComponent } from "./footer/footer.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true, // Only import RouterOutlet here
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    imports: [
-        CommonModule,
-        RouterOutlet,
-        RouterModule,
-        RouterLink,
-        HomeComponent,
-        NavbarComponent,
-        SkillComponent,
-        FooterComponent
-    ]
+  selector: 'app-root',
+  standalone: true, // Only import RouterOutlet here
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    RouterLink,
+    HomeComponent,
+    NavbarComponent,
+    SkillComponent,
+    FooterComponent,
+    RouterLinkActive,
+  ],
 })
 export class AppComponent {
   title = 'BradBurzon';
