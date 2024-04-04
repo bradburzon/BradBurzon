133a77337dc93d445a76bf1fee2f73419da1b0ae
Brad Burzon <bradburzon@ymail.com>
2024-04-04 14:32:12 -0700

    delete skill html, move template inside skill component

--- src/app/skill/skill.component.ts
+++ src/app/skill/skill.component.ts
@@ -4,10 +4,12 @@ import { SkillsComponent } from './skills/skills.component';
 @Component({
   selector: 'app-skill',
   standalone: true,
-  templateUrl: './skill.component.html',
+  template: ` <div class="row">
+    <div class="col-12 main-content">
+      <app-skills></app-skills>
+    </div>
+  </div>`,
   styleUrl: './skill.component.css',
-  imports: [
-    SkillsComponent,
-  ],
+  imports: [SkillsComponent],
 })
 export class SkillComponent {}
